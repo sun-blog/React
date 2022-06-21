c5216c2d84cafef44d0fe65ae136028703dfc8fb
wtx <1575374226@qq.com>
2022-06-21 12:24:02 +0800

    project commit

--- React(尚硅谷)/staging/012_src_Redirect的使用/pages/about/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class index extends Component {
-    render() {
-        return <h3>about页面</h3>;
-    }
-}
--- React(尚硅谷)/staging/05_src_组件通信（壹）/components/receive/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class index extends Component {
-	render() {
-		const { msg } = this.props;
-		return <h1>{msg ? msg : "等待数据发送"}</h1>;
-	}
-}
